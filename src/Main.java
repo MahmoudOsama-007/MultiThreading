2a8d8e7f9cc854e486e8fb53dda1ba7529523ac5
Maxmo_PC <PC@example.com>
2025-04-11 06:19:22 +0200

    create MyRunnable & use to create 3 Threads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+
+//        MyRunnable myRunnable = new MyRunnable();
+//        Thread thread = new Thread(myRunnable);
+
+        // you can pass direct to Thread by using Anonymous Object
+        Thread thread1 = new Thread(new MyRunnable() {});
+        Thread thread2 = new Thread(new MyRunnable() {});
+        Thread thread3 = new Thread(new MyRunnable() {});
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
