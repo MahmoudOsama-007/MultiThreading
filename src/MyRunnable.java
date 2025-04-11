ae47fee8408261384eafafc6049f99993f113859
Maxmo_PC <PC@example.com>
2025-04-11 06:24:13 +0200

    update MyRunnable Class: print thread number

--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -4,7 +4,7 @@ public class MyRunnable implements Runnable {
         for (int i = 1; i <= 5; i++) {
             try {
                 Thread.sleep(1000);
-                System.out.println(i);
+                System.out.println(Thread.currentThread().getName() + " " + i);
             } catch (InterruptedException e) {
                 System.out.println("Thread was interrupted");
             }
