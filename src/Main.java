7a9f1af5f50b0215aa415cff10be4f680c2aa414
Maxmo_PC <PC@example.com>
2025-04-11 06:30:59 +0200

    update MyRunnable Class: Give each Thread name

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ public class Main {
 //        Thread thread = new Thread(myRunnable);
 
         // you can pass direct to Thread by using Anonymous Object
-        Thread thread1 = new Thread(new MyRunnable() {});
-        Thread thread2 = new Thread(new MyRunnable() {});
-        Thread thread3 = new Thread(new MyRunnable() {});
+        Thread thread1 = new Thread(new MyRunnable("ahmed") {});
+        Thread thread2 = new Thread(new MyRunnable("ali") {});
+        Thread thread3 = new Thread(new MyRunnable("mahmoud") {});
+        System.out.println("Call Threads Start");
         thread1.start();
         thread2.start();
         thread3.start();
+        System.out.println("Call Threads End"); // where do you think will it print ?
     }
 }
--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -1,10 +1,17 @@
+import java.security.PrivateKey;
+
 public class MyRunnable implements Runnable {
+    private String name;
+    public MyRunnable(String name) {
+        this.name = name;
+    }
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
             try {
                 Thread.sleep(1000);
-                System.out.println(Thread.currentThread().getName() + " " + i);
+//                System.out.println(Thread.currentThread().getName() + " " + i);
+                System.out.println("From: "+name + " " + i);
             } catch (InterruptedException e) {
                 System.out.println("Thread was interrupted");
             }
